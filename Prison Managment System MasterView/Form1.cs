3430c37ea25f4aa010ef71d87592fd1727dff982
xniccum <xniccum@gmail.com>
2015-01-25 13:12:44 -0500

    Commiting Master Control

---
+++ Prison Managment System MasterView/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prison_Managment_System_MasterView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void login_tb_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=titan.csse.rose-hulman.edu;Database=PMS;Uid="+
+                login_bt.Text+";Pwd="+password_tb.Text+";";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlQuery_tb.Text, connection);
+                SqlDataAdapter sda = new SqlDataAdapter(command);
+                DataTable tb=new DataTable();
+                sda.Fill(tb);
+                datagridView.DataSource=new BindingSource(tb,null);
+        }
+    }
+}
