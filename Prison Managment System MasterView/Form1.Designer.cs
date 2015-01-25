829a7adf9f0e91b6dfb5f96415c3c9d081d77cd4
xniccum <xniccum@gmail.com>
2015-01-25 13:36:06 -0500

    Changed

--- Prison Managment System MasterView/Form1.Designer.cs
+++ Prison Managment System MasterView/Form1.Designer.cs
@@ -34,15 +34,15 @@
             this.login_tb = new System.Windows.Forms.TextBox();
             this.password_tb = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.sqlQuery_tb = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // login_bt
             // 
-            this.login_bt.Location = new System.Drawing.Point(91, 94);
+            this.login_bt.Location = new System.Drawing.Point(15, 93);
             this.login_bt.Name = "login_bt";
-            this.login_bt.Size = new System.Drawing.Size(214, 37);
+            this.login_bt.Size = new System.Drawing.Size(290, 37);
             this.login_bt.TabIndex = 1;
             this.login_bt.Text = "Login";
             this.login_bt.UseVisualStyleBackColor = true;
@@ -51,7 +51,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Location = new System.Drawing.Point(12, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 17);
             this.label1.TabIndex = 2;
@@ -60,7 +60,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Location = new System.Drawing.Point(12, 52);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 17);
             this.label2.TabIndex = 3;
@@ -68,20 +68,24 @@
             // 
             // login_tb
             // 
-            this.login_tb.Location = new System.Drawing.Point(91, 22);
+            this.login_tb.Location = new System.Drawing.Point(91, 12);
             this.login_tb.Name = "login_tb";
             this.login_tb.Size = new System.Drawing.Size(214, 22);
             this.login_tb.TabIndex = 4;
             // 
             // password_tb
             // 
-            this.password_tb.Location = new System.Drawing.Point(91, 57);
+            this.password_tb.Location = new System.Drawing.Point(91, 52);
             this.password_tb.Name = "password_tb";
+            this.password_tb.PasswordChar = '*';
             this.password_tb.Size = new System.Drawing.Size(214, 22);
             this.password_tb.TabIndex = 5;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(15, 148);
             this.dataGridView1.Name = "dataGridView1";
@@ -89,20 +93,20 @@
             this.dataGridView1.Size = new System.Drawing.Size(761, 314);
             this.dataGridView1.TabIndex = 6;
             // 
-            // richTextBox1
+            // sqlQuery_tb
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(317, 25);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(458, 105);
-            this.richTextBox1.TabIndex = 7;
-            this.richTextBox1.Text = "";
+            this.sqlQuery_tb.Location = new System.Drawing.Point(317, 12);
+            this.sqlQuery_tb.Name = "sqlQuery_tb";
+            this.sqlQuery_tb.Size = new System.Drawing.Size(458, 118);
+            this.sqlQuery_tb.TabIndex = 7;
+            this.sqlQuery_tb.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(788, 474);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.sqlQuery_tb);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.password_tb);
             this.Controls.Add(this.login_tb);
@@ -125,7 +129,7 @@
         private System.Windows.Forms.TextBox login_tb;
         private System.Windows.Forms.TextBox password_tb;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox sqlQuery_tb;
     }
 }
 
