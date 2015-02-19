fd9b29ca732baf849f4cc2d889744825617cdd19
Peter Larson <peterlsm4620@gmail.com>
2015-02-18 21:24:51 -0500

    Finished draft of Cell Editor
    Edits cells. can not be opened form the main-form so that me and austin
    do not both modify it at the same time

---
+++ PMSTest/PMSTest/CellEditor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PMSTest
+{
+    public partial class CellEditor : Form
+    {
+        Main_Form parentForm;
+        Panel currentPanel;
+
+        public CellEditor()
+        {
+            InitializeComponent();
+            currentPanel = movePrisonerPanel;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected = comboBox1.SelectedItem.ToString();
+            switch (selected)
+            {
+                case "Add Cell":
+                    focusPanel(addCellPanel);
+                    break;
+                case "Change A Prisoner's Cell":
+                    focusPanel(movePrisonerPanel);
+                    break;
+
+            }
+        }
+        private void hidePanes()
+        {
+            addCellPanel.Visible = false;
+            movePrisonerPanel.Visible = false;
+
+        }
+        private void focusPanel(Panel desired)
+        {
+            hidePanes();
+            desired.Visible = true;
+            Point oldLocation = this.currentPanel.Location;
+            this.currentPanel.Location = desired.Location;
+            desired.Location = oldLocation;
+            this.currentPanel = desired;
+        }
+        private void fillComboBox()
+        {
+            comboBox1.Items.AddRange(new Object[] {
+                "Add Cell",
+                "Change A Prisoner's Cell"
+            });
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
