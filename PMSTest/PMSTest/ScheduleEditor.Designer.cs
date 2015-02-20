317596db06079ca2383ee3def81c2acf65ad25ab
Peter Larson <larsonp@rose-hulman.edu>
2015-02-19 19:40:24 -0500

    Resized window

--- PMSTest/PMSTest/ScheduleEditor.Designer.cs
+++ PMSTest/PMSTest/ScheduleEditor.Designer.cs
@@ -798,7 +798,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1373, 652);
+            this.ClientSize = new System.Drawing.Size(658, 267);
             this.Controls.Add(this.guardSchedules_IUpanel);
             this.Controls.Add(this.jobSchedules_Dpanel);
             this.Controls.Add(this.jobSchedules_IUpanel);
