a9c8d21648b1ddfddb894b3ca237881c1d0169c3
Peter Larson <larsonp@rose-hulman.edu>
2015-02-19 19:31:32 -0500

    Changed all 'manager's to 'editors

--- PMSTest/PMSTest/Main_Form.Designer.cs
+++ PMSTest/PMSTest/Main_Form.Designer.cs
@@ -54,7 +54,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(17, 54);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(57, 39);
             this.button1.TabIndex = 3;
@@ -65,7 +65,7 @@
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(88, 34);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(68, 20);
@@ -87,7 +87,7 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(18, 110);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(500, 197);
@@ -102,7 +102,7 @@
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Location = new System.Drawing.Point(18, 8);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(170, 99);
             this.panel1.TabIndex = 7;
@@ -110,7 +110,7 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(88, 54);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(67, 39);
             this.button2.TabIndex = 4;
@@ -121,7 +121,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(88, 10);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(68, 20);
             this.textBox1.TabIndex = 1;
@@ -142,7 +142,7 @@
             this.panel2.Controls.Add(this.button6);
             this.panel2.Controls.Add(this.comboBox1);
             this.panel2.Location = new System.Drawing.Point(207, 8);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(311, 99);
             this.panel2.TabIndex = 8;
@@ -150,7 +150,7 @@
             // button6
             // 
             this.button6.Location = new System.Drawing.Point(235, 11);
-            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(68, 20);
             this.button6.TabIndex = 12;
@@ -163,7 +163,7 @@
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(14, 11);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(206, 21);
             this.comboBox1.TabIndex = 11;
@@ -175,7 +175,7 @@
             this.scheduleButton.Name = "scheduleButton";
             this.scheduleButton.Size = new System.Drawing.Size(122, 23);
             this.scheduleButton.TabIndex = 9;
-            this.scheduleButton.Text = "Schedule Manager";
+            this.scheduleButton.Text = "Schedule Editor";
             this.scheduleButton.UseVisualStyleBackColor = true;
             this.scheduleButton.Click += new System.EventHandler(this.scheduleButton_Click);
             // 
@@ -185,7 +185,7 @@
             this.prisonerButton.Name = "prisonerButton";
             this.prisonerButton.Size = new System.Drawing.Size(122, 23);
             this.prisonerButton.TabIndex = 10;
-            this.prisonerButton.Text = "Prisoner Manager";
+            this.prisonerButton.Text = "Prisoner Editor";
             this.prisonerButton.UseVisualStyleBackColor = true;
             this.prisonerButton.Click += new System.EventHandler(this.prisonerButton_Click);
             // 
@@ -195,7 +195,7 @@
             this.userButton.Name = "userButton";
             this.userButton.Size = new System.Drawing.Size(122, 23);
             this.userButton.TabIndex = 11;
-            this.userButton.Text = "User Manager";
+            this.userButton.Text = "User Editor";
             this.userButton.UseVisualStyleBackColor = true;
             this.userButton.Click += new System.EventHandler(this.button3_Click_1);
             // 
@@ -205,7 +205,7 @@
             this.cellButton.Name = "cellButton";
             this.cellButton.Size = new System.Drawing.Size(122, 23);
             this.cellButton.TabIndex = 12;
-            this.cellButton.Text = "Cell Manager";
+            this.cellButton.Text = "Cell Editor";
             this.cellButton.UseVisualStyleBackColor = true;
             this.cellButton.Click += new System.EventHandler(this.button4_Click_1);
             // 
@@ -215,7 +215,7 @@
             this.guardButton.Name = "guardButton";
             this.guardButton.Size = new System.Drawing.Size(121, 23);
             this.guardButton.TabIndex = 13;
-            this.guardButton.Text = "Guard Manager";
+            this.guardButton.Text = "Guard Editor";
             this.guardButton.UseVisualStyleBackColor = true;
             this.guardButton.Click += new System.EventHandler(this.button5_Click_1);
             // 
@@ -225,7 +225,7 @@
             this.jobButton.Name = "jobButton";
             this.jobButton.Size = new System.Drawing.Size(121, 23);
             this.jobButton.TabIndex = 14;
-            this.jobButton.Text = "Job Manager";
+            this.jobButton.Text = "Job Editor";
             this.jobButton.UseVisualStyleBackColor = true;
             this.jobButton.Click += new System.EventHandler(this.button7_Click);
             // 
@@ -254,7 +254,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Main_Form";
             this.Text = "Prison Management System";
             this.Load += new System.EventHandler(this.Form1_Load);
